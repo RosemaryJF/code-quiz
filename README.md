d1221fb79af8859838b1040a62c4c1efede55181
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-08-20 21:31:43 +1000

    Creation of start button event listener and function. Beginningof work on run quiz finction. Beginning of work on getAnswers function.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # code-quiz
+
+https://www.codegrepper.com/code-examples/java/create+a+button+using+javascript
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,13 @@
 var body = document.body;
 var startCntnr = document.getElementById("starting-page");
-var startBtn = document.getElementsByClassName("start-button");
+var quizCntnr = document.getElementById("quiz");
+var resultsCntnr = document.getElementById("results");
+
+var startBtn = document.getElementById("starting-page");
 var questionEl = document.getElementById("quiz-question");
 var answerEl = document.getElementById("answer-options");
 var sumbitBtn = document.getElementById("submit-button")
+var answerOptions = document.querySelectorAll("answerOptions");
 var questionsEl = document.getElementById("questions");
 // var answerBtn01 = document.createElement("button");
 // var answerBtn02 = document.createElement("button");
@@ -11,104 +15,46 @@ var questionsEl = document.getElementById("questions");
 // var answerBtn04 = document.createElement("button");
 // var button = document.querySelectorAll("button");
 
-
-function getQuestion() {
-    var output = [];
-    questionsEl.forEach ( 
-        (currentQuestion, questionNumber) => {
-            var answers = [];
-
-            for (Number in currentQuestion.answers){
-                answers.push(
-                    `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${number} :
-                    ${currentQuestion.answers[letter]}
-                  </label>`
-                  );
-            }
-                output.push(
-                    `<div class="question"> ${currentQuestion.question} </div>
-                    <div class="answers"> ${answers.join("")} </div>`
-                );
-            }
-        )
-    };
-
-
-    getQuestion()
-
-
-
-// questionEl.textContent = "Commonly used data types DO NOT include:";
-// answerEl.textContent = ["alerts", "string", "booleans", "numbers"];
-
-// answerBtn01.innerHTML = ("alerts");
-// answerBtn02.innerHTML = ("strings");
-// answerBtn03.innerHTML = ("booleans");
-// answerBtn04.innerHTML = ("numbers");
-
-// body.appendChild(questionEl);
+// answerEl.setAttribute("class", "hide");
 // body.appendChild(answerEl);
 
-// startBtn.addEventListener("click", function(runQuiz) {
-//     runQuiz.preventDefault();
-//     startCntnr.setAttribute("class:", "hide")
-// })
+startBtn.addEventListener("click", function (event) {
+    startBtn = event.target;
+     if ("click")
+     startCntnr.setAttribute("style", "display: none");
+     resultsCntnr.setAttribute("style", "display: none");
+     
+     runQuiz()
+    });
+     
+     
+     
+    function runQuiz() {
+        var currentQuestion = questions[0];
+        (console.log(currentQuestion));
+        questionEl.textContent = currentQuestion.question;
+    };
 
-// runQuiz();
-// body.appendChild(answerBtn01);
-// body.appendChild(answerBtn02);
-// body.appendChild(answerBtn03);
-// body.appendChild(answerBtn04);
-
-
-
-// button.addEventListener("click", function () {
-//     if ("click" === answerBtn01) {
-//         console.log("Correct");
-//     }
-//     else if ("click" !== answerBtn01) {
-//         console.log("Incorrect");
-//     }
-// })
+   
+    function getAnswers() {
+        answerEl.removeAttribute("class", "hide");
+        var currentAnswers = questions[0];
+            (console.log(currentAnswers));
+            answerEl.textContent = currentAnswers.answerOptions;
+        function makeButtons () {
+                var answerButtons = document.createElement("button");
+                for (i = 0; i < answerOptions.length; i++)
+                answerButtons.setAttribute("class", "answerOptions");
+                answerButtons.setAttribute("value", answerOptions);
+        }
+        makeButtons()
+    };
+    
+   
+   getAnswers();
     
 
-// })
+    
 
-// var timer = questionEl.length * 15;
+    
 
-// var listEl = document.querySelectorAll("ol");
-// var questionShownEl = document.getElementById("quiz-question");
-// var answersBtn = document.querySelectorAll("li");
-// var questionsEl = document.getElementById("questions");
-
-// var answerOptionsEl = document.getElementById("options");
-// var answersEl = document.getElementsByClassName("button");
-
-// questionShownEl.textContent = getQuestion();
-// answersEl.textContent = (questions[0(1)]);
-
-// console.log(questionEl);
-// console.log(answersEl);
-
-
-// body.appendChild(answersEl);
-
-// function getQuestion () {
-//     var questionOptionsEl = document.getElementById("question");
-
-//     for (i = 0; i = questions.length; i = ++) {
-//         questionOptionsEl = 
-//     }
-//     var chosenQuestion = questions.length;
-//     console.log(chosenQuestion);
-//     questionShownEl.push(questionChars);
-
-
-//     // for (i = 0; i < questions.length; i++) {
-//         // chosenQuestion.push(questions);
-//     // }
-// }
-
-// body.appendChild(questionShownEl);
