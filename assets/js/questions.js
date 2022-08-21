22d0d156c152233ba911eb79e2d6e0bdb517751e
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-08-21 12:12:21 +1000

    Removal of comment code in questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -31,24 +31,3 @@ var questions = [
     answer: "console.log",
     },
 ];
-
-
-// Example of quiz option using prompts
-// var question = [
-//     ["Commonly used data types DO NOT include:", "strings, booleans", "arrays"],
-//     ["What is the capital of France?", "PARIS", "Remember the eiffel tower?"],
-//     ["What is the capital of Canada?", "OTTAWA", "Lot of mooses there"]
-//     ];
-    
-//     var correctAnswers = 0;
-    
-//     for (var i = 0; i < question.length; i++) {
-//         var answer = prompt(question[i][0]);
-//         if (answer.toUpperCase() == question[i][1]) {
-//             alert("Correct! " + question[i][2]);
-//             correctAnswers++;
-//         }
-//         else {
-//             alert("incorrect, the correct answer was " + question[i][1]);
-//         }
-//     }
